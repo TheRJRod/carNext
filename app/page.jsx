1fd452dd1eb8437d972418ae2623370a6f15c811
TheRJRod <104101723+TheRJRod@users.noreply.github.com>
2023-08-20 11:13:38 -0500

    test

--- app/page.jsx
+++ app/page.jsx
@@ -9,15 +9,15 @@ export default function Home() {
       <>
       <AnimatePresence  mode='wait'>
       
-      <motion.div className='flex flex-col px-20 gap-x-0 gap-y-20 md:gap-y-0 text-[14px] md:text-[16px] xl:flex-row justify-center xl:gap-x-32 pt-10 md:pt-32 items-center '>
-      <div className="flex flex-col  gap-y-4">
+      <motion.div className='flex flex-col px-20 gap-x-0 gap-y-20 md:gap-y-0  xl:flex-row justify-center xl:gap-x-32 pt-10 md:pt-32 items-center wrapper'>
+      <div className="flex flex-col  gap-y-4 text-[14px] md:text-[16px]">
       <h1 className="text-4xl md:text-6xl text-white">YOUR CAR <br/> MADE BY <span className="text-yellow-500">THE FUTURE</span></h1>
       <p className='text-white'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s</p>
     <Link href='/car1details'><button className="border border-white text-white rounded-full py-2 px-4 text-xl self-start hover:bg-yellow-500">View latest model</button></Link>
       </div>
-      <div className='text-[0px] tracking-normal	'>
+      
       <Image className='z-10 scale-150 md:scale-100' src={require('./images/bugatti.png')} width={800} height={800} alt='bugatti' />
-      </div>
+      
       <ParticlesContainer/>
       <motion.div className='slide-in z-20' initial={{scaleY: 0}} animate={{scaleY:0}} exit={{scaleY:1}} transition={{duration: 2, ease: [0.22, 1, 0.36, 1]}}>
 
